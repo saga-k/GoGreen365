bd3bfe46e591e289461cf6fc0388b38dcdf3aa44
saga-k <sagakrusell+1@gmail.com>
2025-02-19 10:42:07 +0000

    addUser works

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,7 +1,30 @@
 <script setup>
+import { useUserStore } from '@/stores/userStore'
+
+const userStore = useUserStore()
+
+let testUser = {
+  firstname: null,
+  lastname: null,
+  email: null,
+  password: null
+}
+
+function addTestUser() {
+  userStore.addUser(testUser)
+  console.log('Users:', userStore.users)
+}
+
 </script>
 
 <template>
   <h1>createAccount</h1>
   <RouterLink to="onboarding">Lägg in länk till onboarding screen här</RouterLink>
+  <input id='firstname' type="text" placeholder="firstname" v-model="testUser.firstname">
+  <input id='lastname' type="text" placeholder="lastname" v-model="testUser.lastname">
+  <input id='email' type="text" placeholder="email" v-model="testUser.email">
+  <input id='password' type="text" placeholder="password" v-model="testUser.password">
+  <button @click="addTestUser">AddUser</button>
+
+
 </template>
