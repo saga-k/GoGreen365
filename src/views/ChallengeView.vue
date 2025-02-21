3ead71da6e7fe848a55fe186f2ca79790a588828
Ecaterina Zhukouskaya <katjas.emailg@gmail.com>
2025-02-21 14:49:15 +0100

    done #1 display loading, error, and challenge data

--- src/views/ChallengeView.vue
+++ src/views/ChallengeView.vue
@@ -15,6 +15,19 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h1>Dagens utmaning</h1>
+  <div class="challenge-container">
+    <h1>Dagens utmaning</h1>
+
+    <div v-if="challengeStore.loading">Loading...</div>
+    <div v-else-if="challengeStore.error">Error: {{ challengeStore.error }}</div>
+
+    <div v-if="challengeStore.todaysChallenge" class="challenge">
+      <h2>{{ challengeStore.todaysChallenge.title }}</h2>
+      <p>{{ challengeStore.todaysChallenge.description }}</p>
+    </div>
+    <div v-else class="no-challenge">
+      <p>Inga fler utmaningar tillgängliga för idag.</p>
+    </div>
+  </div>
   <Navbar page="challenge"></Navbar>
 </template>
