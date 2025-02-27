ec01a10370eb90568f61ec7af74e5c248840a862
Prakaikool <prakaikool.teepraken@gmail.com>
2025-02-27 05:17:12 +0100

    Style the page

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -62,10 +62,8 @@ async function updatePassword() {
 </script>
 
 <template>
-  <div class="">
-    <div class="forgot-app">
-      <img src="@/assets/images/happy-earth.png" alt="Happy Earth" class="earth-image" />
-    </div>
+  <div class="forgot-app">
+    <img src="@/assets/images/happy-earth.png" alt="Happy Earth" class="earth-image" />
 
     <div class="form-container">
       <!-- Change password -->
@@ -87,10 +85,98 @@ async function updatePassword() {
       <p v-if="errorMessages" class="error-message">{{ errorMessage }}</p>
 
       <!-- Push to success page -->
-      <button @click="updatePassword" :disabled="!!errorMessages">Ändra lösenord</button>
+      <button @click="updatePassword" :disabled="!!errorMessages" class="confirm-button">
+        Ändra lösenord
+      </button>
     </div>
     <RouterLink to="/createAccount" class="create-account-link">Skapa nytt konto</RouterLink>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Base style */
+.forgot-app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #fef7ee;
+  color: #3f3d3d;
+  font-family: 'Comfortaa', serif;
+}
+
+/* Image - Logo */
+.earth-image {
+  width: 180px;
+  margin-bottom: 0.5rem;
+}
+
+/* Font style */
+h1 {
+  font-size: 36px;
+  color: #3f3d3d;
+}
+
+/* Form style */
+.form-container {
+  width: 100%;
+  max-width: 300px;
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-container label {
+  margin-bottom: 0.25rem;
+  margin-top: 1rem;
+  font-weight: 600;
+  font-size: 14px;
+  transition: color 0.2s ease;
+}
+
+.form-container input {
+  padding: 0.75rem;
+  margin-bottom: 0.5rem;
+  border: 1px solid #f4dec3;
+  border-radius: 6px;
+  font-size: 14px;
+  outline: none;
+  transition:
+    border-color 0.2s ease,
+    box-shadow 0.2s ease;
+}
+
+/* Focus when click */
+.form-container input:focus {
+  border-color: #c2e07a;
+  box-shadow: 0 0 0 2px rgba(194, 224, 122, 0.3);
+}
+
+/* Underline for every link paegs */
+.create-account-link {
+  display: block;
+  margin-top: 2rem;
+  font-size: 14px;
+  color: #3f3d3d;
+  text-decoration: underline;
+}
+
+.confirm-button {
+  font-family: 'Comfortaa', serif;
+  background-color: #c2e07a;
+  color: #3f3d3d;
+  font-size: 1rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 39px;
+  padding: 0.75rem;
+  cursor: pointer;
+  margin-top: 1rem;
+  transition: background-color 0.3s ease;
+}
+
+.confirm-button:hover {
+  background-color: #a5c261;
+}
+</style>
