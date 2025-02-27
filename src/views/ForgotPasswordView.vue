9c3256c99e91d27cc35366a81d2e07fff1c31875
Prakaikool <prakaikool.teepraken@gmail.com>
2025-02-27 03:34:49 +0100

    Add script function

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -1,4 +1,57 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const userEmail = ref('')
+const newPassword = ref('')
+const confirmPassword = ref('')
+const errorMessages = ref('')
+
+// Validation
+watch([userEmail, newPassword, confirmPassword], ([email, password, confirm]) => {
+  if (!email || !password || !confirm) {
+    errorMessages.value = 'Vänligen fyll i alla fälten.'
+  } else if (password !== confirm) {
+    errorMessages.value = 'Lösenord matchar inte!'
+  } else {
+    errorMessages.value = ''
+  }
+})
+
+// Update password in JSON server
+async function updatePassword() {
+  try {
+    // Find the user by emial on json
+    const response = await fetch(`http://localhost:3000/users?mail=${userEmail.value}`)
+    const users = await response.json()
+
+    if (users.length === 0) {
+      errorMessages.value = 'Fel e-post adress'
+      return
+    }
+
+    const userId = users[0].id
+
+    // Update new password to json
+    const updateResponse = await fetch(`http://localhost:3000/users/${userId}`, {
+      method: 'PATCH',
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify({ password: newPassword.value }),
+    })
+
+    // Push to success page
+    if (updateResponse.ok) {
+      router.push('/successPassword')
+    } else {
+      errorMessages.value = 'Missllyckades med att uppdatera lösenordet.'
+    }
+  } catch (error) {
+    console.error('Error updating password:', error)
+    errorMessages.value = 'Något gick fel. Försök igen.'
+  }
+}
+</script>
 
 <template>
   <div class="">
@@ -22,7 +75,11 @@
       <label for="confirmPassword">bekräfta lösenord</label>
       <input v-model="confirmPassword" type="password" placeholder="Bekräfta lösenord" />
 
-      <button @click="updatePassword">Ändra lösenord</button>
+      <!-- Show up error message -->
+      <p v-if="errorMessages" class="error-message">{{ errorMessage }}</p>
+
+      <!-- Push to success page -->
+      <button @click="updatePassword" :disabled="!!errorMessages">Ändra lösenord</button>
     </div>
     <RouterLink to="/createAccount" class="create-account-link">Skapa nytt konto</RouterLink>
   </div>
